fca8fff86f477bded51338ae023d7ec8ba9b5c83
smiler <34510769+smiler96@users.noreply.github.com>
2020-08-25 10:59:39 +0800

    Update core.py

--- models/common/core.py
+++ models/common/core.py
@@ -1,1 +1,16 @@
+import torch.nn as nn
+
+def conv_default(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+def calc_mean_std(feat, eps=1e-5):
+    # eps is a small value added to the variance to avoid divide-by-zero.
+    size = feat.size()
+    assert (len(size) == 4)
+    N, C = size[:2]
+    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(N, C, 1, 1)
+    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
+    return feat_mean, feat_std
+
 
