7f956defe4a1becec0ad040bdc6567a4ff9971c7
smiler <34510769+smiler96@users.noreply.github.com>
2020-08-25 10:52:26 +0800

    Update loss.py

--- losses/loss.py
+++ losses/loss.py
@@ -1,1 +1,15 @@
+import torch.nn as nn
+from models.common.core import calc_mean_std
 
+def content_loss(input, target, criterion=nn.MSELoss()):
+    assert (input.size() == target.size())
+    # assert (target.requires_grad is False)
+    return criterion(input, target)
+
+def style_loss(input, target, criterion=nn.MSELoss()):
+    assert (input.size() == target.size())
+    # assert (target.requires_grad is False)
+    input_mean, input_std = calc_mean_std(input)
+    target_mean, target_std = calc_mean_std(target)
+    return criterion(input_mean, target_mean) + \
+           criterion(input_std, target_std)
